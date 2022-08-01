3ef1d727b822b81031ae20460ed99c8f758450b7
Matthew <66563291+Sir-Goose@users.noreply.github.com>
2022-08-01 15:55:02 +0200

    Add files via upload

--- pizza.py
+++ pizza.py
@@ -1,16 +1,24 @@
 import csv
 import sys
 
-import tabulate
+import tabulate as tabulate
 
-# menu = sys.argv[1]
-menu = "regular.csv"
+if len(sys.argv) != 2:
+    if len(sys.argv) < 1:
+        sys.exit("Too few command-line arguments")
+    if len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
 
-with open(menu) as file:
-    reader = csv.DictReader(file)
-    print(reader)
-    
+extension = sys.argv[1]
+extension = extension.split('.')
+if extension[1] != "csv":
+    sys.exit("Not a CSV file")
 
+try:
 
+    with open(sys.argv[1]) as file:
+        reader = csv.DictReader(file)
+        print(tabulate.tabulate(reader, headers="keys", tablefmt="grid"))
 
-
+except FileNotFoundError:
+    sys.exit("File does not exist")
